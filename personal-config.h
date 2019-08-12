db7e8e47b4804576541701d339ff04fa48ec94ca
Myc3lium <myc3lium@firemail.cc>
2019-08-12 12:29:15 +0100

    update personal config. update readme

---
+++ personal-config.h
@@ -0,0 +1,74 @@
+// Using arrays for intervals and signals allows us to dispatch
+// events/modules in O(1) (constant) time.
+const char* format_separator = "|";
+#define FORMAT "%s"
+
+
+Event
+on_interval [][MAX_PER_INTERVAL + 1] = {
+	ON_INTERVAL(1,
+		EVENT(
+			.command    = "date '+[%H:%M:%S]'",
+			.on_startup = 1,
+			.order      = 5
+		)
+	),
+
+	ON_INTERVAL(5,
+		EVENT(
+			.command     = "bash ~/.config/dwm/bandwidth",
+			.placeholder = "",
+			.on_startup  = 1,
+			.order       = 2
+		)
+	),
+
+	ON_INTERVAL(10,
+		EVENT(
+			.command    = "acpi -b | awk '{ print $3, substr($4,1,index($4,\"%\")) }'",
+			.on_startup = 1,
+			.order      = 4
+		)
+	),
+
+	ON_INTERVAL(60,
+		EVENT(
+			.command    = "uptime -p | awk '{ print \"up\",$2 \":\" $4 }'",
+			.on_startup = 1,
+			.order      = 3
+		)
+	),
+};
+
+Event
+on_signal [][MAX_PER_SIGNAL + 1] = {
+	ON_SIGNAL(1,
+			EVENT(
+				.command     = "bash \"$HOME/.config/dwm/cmus-blocklet\"",
+				.placeholder = "<cmus down>",
+				.on_startup  = 1,
+				.order       = 0
+			)
+	),
+
+	ON_SIGNAL(2,
+			EVENT(
+				.command     = "bash \"$HOME/.config/dwm/volume\"",
+				.placeholder = "<volume down>",
+				.on_startup  = 1,
+				.order       = 1
+			)
+	),
+
+	{EMPTYEVENT},
+};
+
+Event
+on_startup [] = {
+	EMPTYEVENT
+	// EVENT(
+	// 	.placeholder = "atomstatus vs." VERSION,
+	// 	.on_startup  = 0,
+	// 	.order       = 0
+	// ),
+};
